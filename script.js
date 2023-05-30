2adfd883cdb004cecfa32fc2a1c637ebbcbd0fb4
harryRobinson <127695266+Hazbob@users.noreply.github.com>
2023-05-31 00:19:00 +0100

    added a delete button
    need to add some more functionality so it deletes the added cost but will sort that tomorrow

--- script.js
+++ script.js
@@ -4,20 +4,33 @@ const inputTags = document.querySelectorAll("input");
 const addButton = document.querySelector("#add");
 const container = document.querySelector(".container");
 const totalSpendEl = document.querySelector(".spend-amount");
-
+const deleteEL = document.querySelectorAll(".delete-btn");
 totalSpendEl.innerHTML = ``;
 let totalAmount = 0;
-
-console.log(inputTags);
+totalSpendEl.innerHTML = `£${0}`;
+// function to update total spending:
+// deleteEL.forEach((button) => {
+//   button.addEventListener("click", function (e) {
+//     e.preventDefault();
+//     const grandParentTracker = button.closest(".ticket-info").parentNode;
+//     grandParentTracker.remove();
+//   });
+// });
+//
+// button to click and add the inputs to the rows
 addButton.addEventListener("click", function (e) {
   e.preventDefault();
+  // declare empty array to insert details
   const values = [];
+  // for each loop to push all the values to the array
   inputTags.forEach((input) => {
     values.push(input.value);
     input.value = "";
   });
+  // destructuring to allow me to access each value independently
   const [eventName, eventDate, venue, booking, section, row, seats, cost] =
     values;
+  // the html i will be appending each time using string literals
   const rowHtml = `
 
     <div class="tracker">
@@ -34,9 +47,17 @@ addButton.addEventListener("click", function (e) {
       <div class="row">${row}</div>
       <div class="row">${seats}</div>
       <div class="row">${cost}</div>
+      <div class="row"><button class="delete-btn">Delete</button></div>
         </div>
     </div>`;
   container.insertAdjacentHTML("beforeend", rowHtml);
+  const deleteButton = container.lastElementChild.querySelector(".delete-btn");
+  deleteButton.addEventListener("click", function (e) {
+    e.preventDefault();
+    const grandParentTracker = deleteButton.closest(".tracker");
+    grandParentTracker.remove();
+  });
+
   console.log(values);
   const updateTotalSpend = function () {
     const costNum = Number(cost);
@@ -48,6 +69,14 @@ addButton.addEventListener("click", function (e) {
   };
   updateTotalSpend();
 });
+
+// deleteEL.forEach((button) => {
+//   button.addEventListener("click", function (e) {
+//     e.preventDefault();
+//     const grandParentTracker = button.closest(".tracker").parentNode;
+//     grandParentTracker.remove();
+//   });
+// });
 
 /*
 <div class="tracker">
